c794ba6768c3ce22dfba0d6bf0029b13254a8fb1
pedrocaseiro <pcaseiro96@gmail.com>
2016-01-30 16:40:51 +0000

    login

--- dotworks/dotworksServer/models.py
+++ dotworks/dotworksServer/models.py
@@ -3,3 +3,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	email = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
+
+#class Company(models.Model):
+#	companyUser= models.User()
+	#atributos
+
+
+#class Student(models.Model):
+	#atributos
+
+
+
--- dotworks/dotworksServer/views.py
+++ dotworks/dotworksServer/views.py
@@ -1,8 +1,49 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.template import loader
+
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the dotworksServer index.")
+
+    template = loader.get_template('dotworksServer/landingPage.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
+def user_login(request):
+	state = "Please log in below..."
+	email = ''
+	password = ''
+	context = {
+
+    }
+	
+
+	if request.POST:
+		email = request.POST.get('email')
+		password = request.POST.get('password')
+
+		user = authenticate(username = email, password = password)
+
+		if user is not None: #login succesfull
+			login(request, user)
+			template = loader.get_template('dotworksServer/home.html')
+			return HttpResponse(template.render(context, request))
+		else:
+			template = loader.get_template('dotworksServer/landingPage.html')
+			return HttpResponse(template.render(context, request))
+
+		
+
+
+	
