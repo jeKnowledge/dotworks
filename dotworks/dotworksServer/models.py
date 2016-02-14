0ada0a70471f8f21504a0b87c7915bd73cadf4bb
josemarcelino <jose.m.s.marcelino@gmail.com>
2016-02-14 21:32:13 +0000

    ready to commit

--- dotworks/dotworksServer/models.py
+++ dotworks/dotworksServer/models.py
@@ -1,16 +1,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Company(models.Model):
+	user = models.OneToOneField(
+		User,
+		on_delete= models.CASCADE,
+		primary_key=True,
+	)
+	companyName = models.CharField(max_length=100)
+	website = models.CharField(max_length=100)
 
-#class Company(models.Model):
-#	companyUser= models.User()
-	#atributos
 
-
-#class Student(models.Model):
-	#atributos
-
+class Student(models.Model):
+	user = models.OneToOneField(
+		User,
+		on_delete= models.CASCADE,
+		primary_key=True,
+	)
+	decription = models.CharField(max_length=500)
+	github = models.CharField(max_length=100)
+	linkdin = models.CharField(max_length=100)
+	facebook = models.CharField(max_length=100)
+	phone = models.CharField(max_length=100)
+	name = models.CharField(max_length=100)
+	city = models.CharField(max_length=100)
+	country = models.CharField(max_length=100)
+	age = models.CharField(max_length=100)
+	degree = models.CharField(max_length=100)
 
 
