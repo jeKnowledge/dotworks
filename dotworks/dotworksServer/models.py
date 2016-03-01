641b6a663a9ba5d3f1b6a270a161b90ade79f33b
josemarcelino <jose.m.s.marcelino@gmail.com>
2016-03-01 12:06:40 +0000

    company model

--- dotworks/dotworksServer/models.py
+++ dotworks/dotworksServer/models.py
@@ -5,7 +5,19 @@ from django.core.validators import RegexValidator
 # Create your models here.
 class Company(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	company = models.CharField(max_length=100)
+	company = models.CharField(max_length=100,blank= False)
+	user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
+	e_mail = models.EmailField(blank = False, unique = True)
+	description = models.TextField(max_length = 500, blank = False)
+	
+	website = models.URLField(max_length=100, blank = True)
+	facebook = models.URLField(max_length=100, blank = True)
+	
+	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+	phone_number = models.CharField(validators=[phone_regex],max_length=15, blank=False) # validators should be a list
+	
+	city = models.CharField(max_length=100, blank = False)
+	country = models.CharField(max_length=100, blank = False)
 
 class Student(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
