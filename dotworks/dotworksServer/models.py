03631b3427a2bf75ad2e09c8bb0a564730f490d0
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-03-05 16:34:14 +0000

    Fixes identation to 4 spaces

--- dotworks/dotworksServer/models.py
+++ dotworks/dotworksServer/models.py
@@ -4,52 +4,52 @@ from django.core.validators import RegexValidator
 
 # Create your models here.
 class Company(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
-	company = models.CharField(max_length=100,blank= False)
-	e_mail = models.EmailField(blank = False, unique = True)
-	description = models.TextField(max_length = 500, blank = False)
-	
-	website = models.URLField(max_length=100, blank = True)
-	facebook = models.URLField(max_length=100, blank = True)
-	
-	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex],max_length=15, blank=False) # validators should be a list
-	
-	city = models.CharField(max_length=100, blank = False)
-	country = models.CharField(max_length=100, blank = False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
+    company = models.CharField(max_length=100,blank= False)
+    e_mail = models.EmailField(blank = False, unique = True)
+    description = models.TextField(max_length = 500, blank = False)
+    
+    website = models.URLField(max_length=100, blank = True)
+    facebook = models.URLField(max_length=100, blank = True)
+    
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex],max_length=15, blank=False) # validators should be a list
+    
+    city = models.CharField(max_length=100, blank = False)
+    country = models.CharField(max_length=100, blank = False)
 
 class Student(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
-	e_mail = models.EmailField(blank = False, unique = True)
-	description = models.TextField(max_length = 500, blank = False)
-	
-	github = models.URLField(max_length=100, blank = True)
-	linkdin = models.URLField(max_length=100, blank = True)
-	facebook = models.URLField(max_length=100, blank = True)
-	
-	phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
-	phone_number = models.CharField(validators=[phone_regex],max_length=15, blank=False) # validators should be a list
-	
-	name = models.CharField(max_length=100, blank = False)
-	
-	city = models.CharField(max_length=100, blank = False)
-	country = models.CharField(max_length=100, blank = False)
-	
-	birth_date = models.DateField(blank = False)
-	
-	#Capital because of convention in django docs
-	DEGREE_CHOICES = (
-		("SECUNDARIO", "Secundario"),
-		("LICENCIATURA", "Licenciatura"),
-		("MESTRADO", "Mestrado"),
-		("DOUTORAMENTO", "Doutoramento"),
-	)
-	degree = models.CharField(max_length=100, choices = DEGREE_CHOICES, blank = False)
-	#NOT SUPPOSED TO BE BLANK=TRUE! BUG
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
+    e_mail = models.EmailField(blank = False, unique = True)
+    description = models.TextField(max_length = 500, blank = False)
+    
+    github = models.URLField(max_length=100, blank = True)
+    linkdin = models.URLField(max_length=100, blank = True)
+    facebook = models.URLField(max_length=100, blank = True)
+    
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex],max_length=15, blank=False) # validators should be a list
+    
+    name = models.CharField(max_length=100, blank = False)
+    
+    city = models.CharField(max_length=100, blank = False)
+    country = models.CharField(max_length=100, blank = False)
+    
+    birth_date = models.DateField(blank = False)
+    
+    #Capital because of convention in django docs
+    DEGREE_CHOICES = (
+        ("SECUNDARIO", "Secundario"),
+        ("LICENCIATURA", "Licenciatura"),
+        ("MESTRADO", "Mestrado"),
+        ("DOUTORAMENTO", "Doutoramento"),
+    )
+    degree = models.CharField(max_length=100, choices = DEGREE_CHOICES, blank = False)
+    #NOT SUPPOSED TO BE BLANK=TRUE! BUG
 
 
 class internship (models.Model):
-	company = models.ForeignKey(Company, on_delete=models.CASCADE)
-	name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
 
 
--- dotworks/dotworksServer/views.py
+++ dotworks/dotworksServer/views.py
@@ -19,92 +19,83 @@ from .models import Company, Student
 
 
 def index(request):
-	if request.user.is_authenticated():
-		template = loader.get_template('dotworksServer/home.html')
-		context = {}
-	else:
-		template = loader.get_template('dotworksServer/landingPage.html')
-		loginForm = LoginForm()
-		context = {
-			'loginForm':loginForm
-		}
-	return HttpResponse(template.render(context, request))
+    if request.user.is_authenticated():
+        template = loader.get_template('dotworksServer/home.html')
+        context = {}
+    else:
+        template = loader.get_template('dotworksServer/landingPage.html')
+        loginForm = LoginForm()
+        context = {
+            'loginForm':loginForm
+        }
+    return HttpResponse(template.render(context, request))
 
 
 
 def user_login(request):
-	context = { }
+    context = { }
 
 
-	if request.POST:
-		form = LoginForm(request.POST)
-		if form.is_valid():
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password']
-			print("%s %s" %(username, password))
-
-			print(username)
-			print(password)
-
-			user = authenticate(username=username, password=password)
-			print(user)
-
-			if user is not None: #login succesfull
-				login(request, user)
-				template = loader.get_template('dotworksServer/home.html')
-				return HttpResponseRedirect(reverse('index'))
-			else:
-				return HttpResponseRedirect(reverse('index'))
-
-		else:
-			return HttpResponseRedirect(reverse('index'))
-
-	else:
-		return HttpResponseRedirect(reverse('index'))
+    if request.POST:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None: #login succesfull
+                login(request, user)
+                template = loader.get_template('dotworksServer/home.html')
+                return HttpResponseRedirect(reverse('index'))
+            else:
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        return HttpResponseRedirect(reverse('index'))
 
 def user_logout(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('index'))
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
 
 def register_action(request):
-	context = {}
-	if request.POST:
-		form = StudentRegisterForm(request.POST)
-		if form.is_valid():
+    context = {}
+    if request.POST:
+        form = StudentRegisterForm(request.POST)
+        if form.is_valid():
 
-			name = form.cleaned_data['name']
-			email = form.cleaned_data['email']
-			password = form.cleaned_data['password']
-			description = form.cleaned_data['description']
-			github = form.cleaned_data['github']
-			linkdin = form.cleaned_data['linkdin']
-			facebook = form.cleaned_data['facebook']
-			phone = form.cleaned_data['phone']
-			city = form.cleaned_data['city']
-			country = form.cleaned_data['country']
-			birth_date = form.cleaned_data['birth_date']
-			degree = form.cleaned_data['degree']
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            description = form.cleaned_data['description']
+            github = form.cleaned_data['github']
+            linkdin = form.cleaned_data['linkdin']
+            facebook = form.cleaned_data['facebook']
+            phone = form.cleaned_data['phone']
+            city = form.cleaned_data['city']
+            country = form.cleaned_data['country']
+            birth_date = form.cleaned_data['birth_date']
+            degree = form.cleaned_data['degree']
 
-			#create new user and student with that user
-			user = User.objects.create_user(username = email, password=password)
-			student = Student(user=user, name = name, e_mail=email, description=description, github=github,linkdin=linkdin, facebook=facebook,
-				phone_number=phone, city=city, country=country, birth_date=birth_date, degree=degree)
-			student.save()
-			user = authenticate(username = email, password = password)
-			if user is not None:
-				login(request, user)
-				template = loader.get_template('dotworksServer/home.html')
-				return HttpResponse(template.render(context, request))
-		else:
-			print(form.errors.as_data())
-			return HttpResponseRedirect(reverse('index'))
-	else:
-		return HttpResponseRedirect(reverse('index'))
+            #create new user and student with that user
+            user = User.objects.create_user(username = email, password=password)
+            student = Student(user=user, name = name, e_mail=email, description=description, github=github,linkdin=linkdin, facebook=facebook,
+                              phone_number=phone, city=city, country=country, birth_date=birth_date, degree=degree)
+            student.save()
+            user = authenticate(username = email, password = password)
+            if user is not None:
+                login(request, user)
+                template = loader.get_template('dotworksServer/home.html')
+                return HttpResponse(template.render(context, request))
+        else:
+            print(form.errors.as_data())
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        return HttpResponseRedirect(reverse('index'))
 
 def student_register(request):
-	template = loader.get_template('dotworksServer/studentRegister.html')
-	studentRegisterForm = StudentRegisterForm()
-	context = {
-		'studentRegisterForm':studentRegisterForm
-	}
-	return HttpResponse(template.render(context, request))		
+    template = loader.get_template('dotworksServer/studentRegister.html')
+    studentRegisterForm = StudentRegisterForm()
+    context = {
+        'studentRegisterForm':studentRegisterForm
+    }
+    return HttpResponse(template.render(context, request))        
