d1118a5dae8b5700b0dbdf304c553c8eac2d9912
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-03-22 15:17:13 +0000

    Makes Internships, Comapnies and students available to modifications in the admin app

--- dotworks/dotworks/urls.py
+++ dotworks/dotworks/urls.py
@@ -1,4 +1,4 @@
-"""dotworks URL Configuration
+"""dotworkslite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^dotworks/', include('dotworksServer.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^dotworks/', include('dotworksServer.urls')),
 ]
--- dotworks/dotworksServer/admin.py
+++ dotworks/dotworksServer/admin.py
@@ -1,2 +1,7 @@
 from django.contrib import admin
 
+from .models import Company, Student, Internship
+
+admin.site.register(Company)
+admin.site.register(Student)
+admin.site.register(Internship)
