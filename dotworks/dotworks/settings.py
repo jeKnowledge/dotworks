74262b06cdb8a7a8533dfa49a848407584411228
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-03-22 15:14:11 +0000

    Corrects installed apps to correctly add dotworksServer appp

--- dotworks/dotworks/settings.py
+++ dotworks/dotworks/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dotworksServer',
+    'dotworksServer.apps.DotworksserverConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'django_extensions',    
+    'django_extensions',    
 ]
 
 MIDDLEWARE_CLASSES = [
