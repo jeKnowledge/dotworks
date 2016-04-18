82d9855254b92bab0595b2cb30b0269c7100a5a7
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-04-18 15:49:19 +0100

    Makes Inscription objects modifiable by admin

--- dotworks/dotworksServer/admin.py
+++ dotworks/dotworksServer/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Company, Student, Internship
+from .models import Company, Student, Internship, Inscription
 
 admin.site.register(Company)
 admin.site.register(Student)
 admin.site.register(Internship)
+admin.site.register(Inscription)
