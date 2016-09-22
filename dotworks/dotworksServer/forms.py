7f741c19924b4d972694798a0bf1dcf829b2a999
Kabombom <mantonio@student.dei.uc.pt>
2016-09-22 15:55:18 +0100

    Fixes some code style errors

--- dotworks/dotworksServer/forms.py
+++ dotworks/dotworksServer/forms.py
@@ -3,57 +3,93 @@ from django import forms
 from .models import Student, Internship, Inscription
 import datetime
 
+
 class LoginForm(forms.Form):
-    username = forms.CharField(label='username',max_length=100)
+    username = forms.CharField(label='username', max_length=100)
     password = forms.CharField(widget=forms.PasswordInput())
 
 class StudentRegisterForm(forms.Form):
     name = forms.CharField(
-        max_length=100, label="Nome Completo", required = True)
-    email = forms.EmailField(required = True)
+        max_length=100, label="Nome Completo", required=True)
+    email = forms.EmailField(required=True)
     password = forms.CharField(
-        widget=forms.PasswordInput(), label="Introduza a sua password", 
-        required = True)
+        widget=forms.PasswordInput(),
+        label="Introduza a sua password",
+        required=True
+    )
     description = forms.CharField(
-        widget= forms.Textarea, max_length = 500, 
-        label="Um texto sobre si [max:500car]", required = True)
-    github = forms.URLField(max_length = 100, label="Github", required = False)
-    linkdin = forms.URLField(max_length=100, label="Linkdin", required = False)
+        widget=forms.Textarea,
+        max_length=500,
+        label="Um texto sobre si [max:500car]",
+        required=True
+    )
+    github = forms.URLField(max_length=100, label="Github", required=False)
+    linkdin = forms.URLField(max_length=100, label="Linkdin", required=False)
     facebook = forms.URLField(
-        max_length=100, label="Facebook", required = False)
+        max_length=100,
+        label="Facebook",
+        required=False
+    )
     phone = forms.RegexField(
-        regex=r'^\+?1?\d{9,15}$', max_length=15, min_length=9, 
-        label="Telemovel", required = True)
-    city = forms.CharField(max_length=100, label="Cidade", required = True)
-    country = forms.CharField(max_length=100, label="Pais", required = True)
+        regex=r'^\+?1?\d{9,15}$',
+        max_length=15,
+        min_length=9,
+        label="Telemovel",
+        required=True
+    )
+    city = forms.CharField(max_length=100, label="Cidade", required=True)
+    country = forms.CharField(max_length=100, label="Pais", required=True)
     birth_date = forms.DateField(
-        widget=forms.DateInput(),label="Data nascimento", 
-        required = True, initial=datetime.date.today)
+        widget=forms.DateInput(),
+        label="Data nascimento",
+        required=True,
+        initial=datetime.date.today
+    )
     degree = forms.ChoiceField(
-        choices = Student.DEGREE_CHOICES, required = True)
+        choices=Student.DEGREE_CHOICES,
+        required=True
+    )
 
 class CompanyRegisterForm(forms.Form):
     company = forms.CharField(
-        max_length = 100, label="Nome da Companhia", required = True)
-    email = forms.EmailField(required = True)
+        max_length=100,
+        label="Nome da Companhia",
+        required = True
+    )
+    email = forms.EmailField(required=True)
     password = forms.CharField(
-        widget=forms.PasswordInput(), label="Password", required = True)
+        widget=forms.PasswordInput(),
+        label="Password",
+        required=True
+    )
     description = forms.CharField(
-        widget= forms.Textarea, max_length = 500, label="Descricao[500car]",
-        required = True)
+        widget=forms.Textarea,
+        max_length=500,
+        label="Descricao[500car]",
+        required=True
+    )
 
     site = forms.URLField(
-        max_length = 100, label="Site da empresa", required = True)
+        max_length=100,
+        label="Site da empresa",
+        required=True
+    )
     facebook = forms.URLField(max_length=100, label="Facebook", required=False)
     phone = forms.RegexField(
-        regex=r'^\+?1?\d{9,15}$', max_length=15, min_length=9, 
-        label="Telemovel", required = True)
+        regex=r'^\+?1?\d{9,15}$',
+        max_length=15,
+        min_length=9,
+        label="Telemovel",
+        required=True
+    )
 
 class InternshipCreationForm(forms.Form):
     title = forms.CharField(max_length=100, label="Titulo do estágio")
-    description = forms.CharField(max_length = 200, label = "Descrição do estágio") 
-    application_deadline = forms.DateField(widget = forms.SelectDateWidget,
-        label = "Data limite para submissão de candidaturas")
+    description = forms.CharField(max_length=200, label="Descrição do estágio")
+    application_deadline = forms.DateField(
+        widget=forms.SelectDateWidget,
+        label = "Data limite para submissão de candidaturas"
+    )
     """
     beggining_date = forms.DateField('Beggining date', required = False)
     duration = forms.IntegerField(choices = Internship.MONTHS_CHOICES)
