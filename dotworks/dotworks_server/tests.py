72e17d96ec2ea52612988470895e8e9a75bdf728
Kabombom <mantonio@student.dei.uc.pt>
2016-11-06 01:44:06 +0000

    Starts creation of custom tests

--- dotworks/dotworks_server/tests.py
+++ dotworks/dotworks_server/tests.py
@@ -1,1 +1,37 @@
-# Create your tests here.
+import datetime
+from django.test import TestCase
+from dotworks_server.models import Student
+
+
+class StudentTestCase(TestCase):
+    """
+    Student creation tests
+    """
+    def create_student(self):
+        """
+        Creates a student
+        """
+        user_id = 7
+        name = 'Machado'
+        email = 'machado@machado.com'
+        description = 'This is a test'
+        phone_number = '987654321'
+        city = 'Lisbon'
+        country = 'Portugal'
+        birth_date = datetime.datetime.now().date()
+        degree = 'SECUNDARIO'
+        return Student.objects.create(
+            user_id=0,
+            name=name,
+            e_mail=email,
+            description=description,
+            phone_number=phone_number,
+            city=city,
+            country=country,
+            birth_date=birth_date,
+            degree=degree
+        )
+
+    def test_student(self):
+        student = self.create_student()
+        self.assertTrue(isinstance(student, Student))
