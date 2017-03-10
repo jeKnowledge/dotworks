f27d661fee84b2c4c9f788cf9f1e30dc622ff78f
Kabombom <mantonio@student.dei.uc.pt>
2017-03-10 17:34:25 +0000

    Tests deployment settings

--- dotworks/dotworks/settings.py
+++ dotworks/dotworks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x)_t7vlb)pv^2=ln_%4tc=2w=#i6ll
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
