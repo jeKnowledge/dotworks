a35da392bd25b19661cf1cba5981f26eab9ba708
Kabombom <mantonio@student.dei.uc.pt>
2017-03-11 22:12:43 +0000

    Changes security settings

--- dotworks/settings.py
+++ dotworks/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'x)_t7vlb)pv^2=ln_%4tc=2w=#i6llo1zltly!)n4_*bi*c5=%')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = bool(os.environ.get('DJANGO_DEBUG'))
 
 ALLOWED_HOSTS = ['*']
 
